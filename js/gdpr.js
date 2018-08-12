dccf6e42dd375b96f4bc0c64a90f71d769d31d88
cryptoverted <crypto@cryptoverted.com>
2018-08-12 15:00:11 +0200

    new buid

---
+++ js/gdpr.js
@@ -0,0 +1,14 @@
+window.addEventListener("load", function(){
+  window.cookieconsent.initialise({
+    "palette": {
+      "popup": {
+        "background": "#373a47"
+      },
+      "button": {
+        "background": "#004a94"
+      }
+    },
+    "theme": "edgeless",
+    "position": "bottom-right"
+  })
+});
